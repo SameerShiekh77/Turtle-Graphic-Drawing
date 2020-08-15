2b0947a60884e7443874a9d350c669213effd3ff
Muhammad_Sameer <muhammadsameer.css@gmail.com>
2020-08-15 17:34:26 +0500

    Rectangle Drawing

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor('Pink')
+
+rec =  turtle.Turtle()
+rec.pensize(width=8)
+rec.color('blue')
+
+rec.forward(150)
+rec.left(90)
+rec.forward(75)
+rec.left(90)
+rec.forward(150)
+rec.left(90)
+rec.forward(75)
+
+wn.exitonclick()
